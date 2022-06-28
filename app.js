06840f3ff0a7a66fe32e23fb491a24f16d78ede0
Theo <95878514+TheoW03@users.noreply.github.com>
2022-06-28 13:01:19 -0400

    added ability to have CSS,html

--- app.js
+++ app.js
@@ -1,57 +1,21 @@
 const http = require('http')
 var fs = require('fs');
 var url = require('url');
-
-// const request = require('request');
+var express = require('express');
+const exp = require('constants');
+const app = express();
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-
-const server = http.createServer((request, response) => {
-    var path = url.parse(request.url).pathname;
-    switch (path) {
-        case '/':
-            response.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            response.write("This is Test Message.");
-            response.end();
-            break;
-        case '/index.html':
-            fs.readFile(__dirname + path, function (error, data) {
-                if (error) {
-                    response.writeHead(404);
-                    response.write(error);
-                    response.end();
-                } else {
-                    response.writeHead(200, {
-                        'Content-Type': 'text/html'
-                    });
-                    response.write(data);
-                    response.end();
-                }
-            });
-            break;
-        case '/nextpage.html':
-            fs.readFile(__dirname + path, function (error, data) {
-                if (error) {
-                    response.writeHead(404);
-                    response.write(error);
-                    response.end();
-                } else {
-                    response.writeHead(200, {
-                        'Content-Type': 'text/html'
-                    });
-                    response.write(data);
-                    response.end();
-                }
-            });
-
-
-    }
+app.use(express.static('frontend'));
+app.use('/css',express.static(__dirname + 'frontend/css'));
+app.use('/js',express.static(__dirname + 'frontend/js'));
+app.use('/img',express.static(__dirname + 'frontend/img'));
+app.get('',(req,res) => {
+    res.sendFile(__dirname + 'frontend/index.html');
+    
 })
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/index.html`)
-})
+app.listen(port, () => console.log(`https://localhost:3000/`));
+
 // server.listen(8082);  
